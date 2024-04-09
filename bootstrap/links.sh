e7d170d5d6f5b879292883f395e4c74bd589adf5
Iussaeus <feluchiy@gmail.com>
2024-04-09 13:01:34 +0300

    added bootstrap scripts folder and small stuff

---
+++ bootstrap/links.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ln -s /home/john/.dotfiles/i3 /home/john/.config/
+ln -s /home/john/.dotfiles/i3-layout-manager /home/john/.config/
+ln -s /home/john/.dotfiles/kitty /home/john/.config/
+ln -s /home/john/.dotfiles/nvim /home/john/.config/
+ln -s /home/john/.dotfiles/scripts /home/john/
+ln -s /home/john/.dotfiles/Pictures /home/john/
