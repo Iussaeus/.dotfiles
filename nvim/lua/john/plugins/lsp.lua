d70f6b11a4e346228177221c7c7656b9d0bedea7
Iussaeus <118941666+Iussaeus@users.noreply.github.com>
2025-03-02 20:01:52 +0000

    changed the lsp setting so the diagnostic are shown after a file save

--- nvim/lua/john/plugins/lsp.lua
+++ nvim/lua/john/plugins/lsp.lua
@@ -86,12 +86,15 @@ return {
 		config = function()
 			local lsp = require 'lspconfig'
 			local capabilities = require 'cmp_nvim_lsp'.default_capabilities()
+
 			-- This is where all the LSP shenanigans will live
 			local lsp_zero = require('lsp-zero')
 			lsp_zero.extend_lspconfig()
 
 			lsp_zero.on_attach(function(client, bufnr)
 				local opts = { buffer = bufnr, remap = false }
+
+				vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ 0 }), { 0 })
 
 				vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
 				vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
@@ -106,11 +109,99 @@ return {
 				vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
 				vim.keymap.set("i", "<A-h>", function() vim.lsp.buf.signature_help() end, opts)
 				vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format() end, opts)
-				vim.keymap.set("n", '<leader>i',
-					function()
-						vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ 0 }), { 0 })
+				vim.keymap.set("n", "<leader>i",
+					function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ 0 }), { 0 }) end, opts)
+
+				-- Diagnostics
+				-- vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+				-- 	vim.lsp.diagnostic.on_publish_diagnostics,
+				-- 	{
+				-- 		virtual_text = false,
+				-- 		signs = false,
+				-- 		update_in_insert = false,
+				-- 		underline = false,
+				-- 	})
+
+				local g = vim.api.nvim_create_augroup("my_group", {});
+
+				vim.api.nvim_create_autocmd("BufEnter", {
+					buffer = bufnr,
+					group = g,
+					callback = function()
+						vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+							vim.lsp.diagnostic.on_publish_diagnostics,
+							{
+								virtual_text = false,
+								signs = false,
+								update_in_insert = false,
+								underline = false,
+							})
+					end,
+				})
+
+				vim.api.nvim_create_autocmd("BufWritePost", {
+					buffer = bufnr,
+					group = g,
+					callback = function()
+						vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+							vim.lsp.diagnostic.on_publish_diagnostics,
+							{
+								virtual_text = true,
+								signs = true,
+								update_in_insert = false,
+								underline = true,
+							})
 					end
-				)
+				})
+
+				vim.api.nvim_create_autocmd("InsertEnter", {
+					buffer = bufnr,
+					group = g,
+					callback = function()
+						vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+							vim.lsp.diagnostic.on_publish_diagnostics,
+							{
+								virtual_text = false,
+								signs = false,
+								update_in_insert = false,
+								underline = false,
+							})
+					end,
+				})
+
+				vim.api.nvim_create_autocmd("InsertLeave", {
+					buffer = bufnr,
+					group = g,
+					callback = function()
+						vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+							vim.lsp.diagnostic.on_publish_diagnostics,
+							{
+								virtual_text = false,
+								signs = false,
+								update_in_insert = false,
+								underline = false,
+							})
+					end,
+				})
+
+				local shader_group = vim.api.nvim_create_augroup("shader", { clear = true })
+
+				local function Gdshader()
+					vim.lsp.start {
+						name = "gdshader",
+						cmd = { "gdshader-lsp" },
+						capabilities = capabilities,
+					}
+				end
+
+				vim.api.nvim_create_autocmd("BufEnter",
+					{
+						callback = function()
+							Gdshader()
+						end,
+						group = shader_group,
+						pattern = { "*.gdshader" },
+					})
 			end)
 
 
@@ -121,6 +212,7 @@ return {
 				}
 
 			})
+
 			-- (Optional) Configure lua language server for neovim
 			local lua_opts = lsp_zero.nvim_lua_ls()
 			require('lspconfig').lua_ls.setup(lua_opts)
@@ -128,27 +220,6 @@ return {
 			lsp.gdscript.setup {
 				filetypes = { "gd", "gdscript", "gdscript3" },
 			}
-
-			function Inlay()
-				vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ 0 }), { 0 })
-			end
-
-			local shader_group = vim.api.nvim_create_augroup("shader", { clear = true })
-
-			vim.api.nvim_create_autocmd("BufEnter",
-				{
-					command = ":lua Gdshader()",
-					group = shader_group,
-					pattern = { "*.gdshader" },
-				})
-
-			function Gdshader()
-				vim.lsp.start {
-					name = "gdshader",
-					cmd = { "gdshader-lsp" },
-					capabilities = capabilities,
-				}
-			end
 
 			require 'lspconfig'.clangd.setup {
 				filetypes = { "c", "cpp", "cc", "mpp", "ixx", "conf" },
@@ -261,16 +332,6 @@ return {
 				on_attach = Inlay(),
 			})
 
-			-- Diagnostics
-			vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-
-				vim.lsp.diagnostic.on_publish_diagnostics,
-				{
-					virtual_text = true,
-					signs = true,
-					update_in_insert = true,
-					underline = true,
-				})
 
 			lsp.gopls.setup({
 				settings = {
