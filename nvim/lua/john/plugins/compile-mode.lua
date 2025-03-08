28ca21e149d9d6ed5bf9ef0157b568dd4f72e705
Iussaeus <118941666+Iussaeus@users.noreply.github.com>
2025-03-08 09:55:45 +0200

    moved to oil

--- nvim/lua/john/plugins/compile-mode.lua
+++ nvim/lua/john/plugins/compile-mode.lua
@@ -1,23 +1,16 @@
 return {
 	"ej-shafran/compile-mode.nvim",
-	tag = "v5.5.0",
-	-- you can just use the latest version:
-	-- branch = "latest",
-	-- or the most up-to-date updates:
-	-- branch = "nightly",
+	vesion = "5.*",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		-- if you want to enable coloring of ANSI escape codes in
-		-- compilation output, add:
 		{ "m00qek/baleia.nvim", tag = "v1.3.0" },
 	},
 	config = function()
 		---@type CompileModeOpts
 		vim.g.compile_mode = {
-			-- to add ANSI escape code support, add:
 			baleia_setup = true,
 			default_command = "",
 		}
-		vim.keymap.set("n", "<space>cm", "<cmd>botright :Compile<cr>");
+		vim.keymap.set("n", "<space>cm", ":botright :Compile");
 	end
 }
--- nvim/lua/john/plugins/oil.lua
+++ nvim/lua/john/plugins/oil.lua
@@ -10,23 +10,19 @@ return {
 		},
 		delete_to_trash = true,
 		view_options = {
-			-- Show files and directories that start with "."
 			show_hidden = true,
 			natural_order = "fast",
-			-- Sort file and directory names case insensitive
-			case_insensitive = false,
-			-- Customize the highlight group for the file name
-			highlight_filename = function(entry, is_hidden, is_link_target, is_link_orphan)
-				return nil
-			end,
+		},
+		lsp_file_methods = {
+			enabled = true,
+			timeout_ms = 1000,
+			autosave_changes = false,
 		},
 	},
-	-- Optional dependencies
-	dependencies = { { "echasnovski/mini.icons", opts = {} } },
-	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
 	lazy = false,
-	init = function(opts)
+	init = function()
 		vim.keymap.set("n", "<leader>pv", "<cmd>Oil<cr>")
 	end
 }
