a3f311e5a685655f97321acffab37d4ddcbe8a31
Iussaeus <118941666+Iussaeus@users.noreply.github.com>
2025-09-08 10:11:23 +0300

    pulled changes from main

--- stow/nvim/.config/nvim/after/ftplugin/c.lua
+++ stow/nvim/.config/nvim/after/ftplugin/c.lua
@@ -1,4 +1,11 @@
+<<<<<<< HEAD
 vim.opt_local.expandtab = true
 vim.opt_local.softtabstop = 4
 vim.opt_local.shiftwidth = 4
 vim.opt_local.tabstop = 4
+=======
+vim.opt_local.tabstop = 4
+vim.opt_local.softtabstop = 4
+vim.opt_local.shiftwidth = 4
+vim.opt_local.expandtab = true
+>>>>>>> 80a0796 (pulled changes from main)
