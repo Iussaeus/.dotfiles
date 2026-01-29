6832b404912ce4caf565d016689cf5cadcb1d454
Iussaeus <118941666+Iussaeus@users.noreply.github.com>
2026-01-29 15:43:27 +0200

    Move spotify and cpu info into the i3blocks config

--- stow-wm/i3/.config/i3blocks/spotify/spotify.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-status=$(playerctl -p spotify status)
-if [ "$status" == "Playing" ];  then
-	metadata=$(playerctl -p spotify metadata --format "{{artist}} - {{title}} {{duration(position)}}")
-	echo $metadata
-fi
