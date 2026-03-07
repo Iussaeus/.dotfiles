925e5c4a9a063c3bcfbd6fabe1fd883bc24a99ac
Iussaeus <118941666+Iussaeus@users.noreply.github.com>
2026-03-07 17:57:01 +0200

    Update sway config

---
+++ stow-wm/sway/.config/sway/scripts/wofi_exec_scripts.sh
@@ -0,0 +1,19 @@
+#!/bin/env bash
+
+all_entries=$(find "$HOME/.local/bin/" -type l -or -type f -executable)
+selected=$(find "$HOME/.local/bin/" -type l -o -type f -executable | xargs -n1 basename | wofi -d)
+selected_full_path=$(grep $selected <<< "$all_entries")
+
+if [ -n "$selected_full_path" ] && [ -e "$selected_full_path" ]; then
+	terms=(kitty foot alacritty)
+	for t in ${terms[*]}
+	do
+		if [ $(command -v $t) ]
+		then
+			detected_term=$t
+			break
+		fi
+	done
+
+	[ -n "$detected_term" ] && exec "$detected_term" -e "$selected_full_path"
+fi
