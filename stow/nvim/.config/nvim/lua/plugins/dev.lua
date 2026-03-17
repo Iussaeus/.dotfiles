e1063f54ca7b42628e6a41ea96b0f7d94361eaa9
Iussaeus <118941666+Iussaeus@users.noreply.github.com>
2026-03-17 17:56:27 +0200

    update local plugin config

--- stow/nvim/.config/nvim/lua/plugins/dev.lua
+++ stow/nvim/.config/nvim/lua/plugins/dev.lua
@@ -1,20 +1,19 @@
 return {
   {
     "Iussaeus/ido-mode.nvim",
-    dir = "~/code/ido-mode.nvim",
     init = function()
-      vim.keymap.set("c", "<c-t>", function() require 'ido-mode'.toggle() end)
-      vim.keymap.set("c", "<c-n>", function() require 'ido-mode'.next_suggestion() end)
-      vim.keymap.set("c", "<c-p>", function() require 'ido-mode'.previous_suggestion() end)
-      vim.keymap.set("c", "<c-y>", function() require 'ido-mode'.accept_suggestion() end)
+      vim.keymap.set("c", "<c-t>", require 'ido-mode'.toggle)
+      vim.keymap.set("c", "<c-n>", require 'ido-mode'.next_suggestion)
+      vim.keymap.set("c", "<c-p>", require 'ido-mode'.previous_suggestion)
+      vim.keymap.set("c", "<c-y>", require 'ido-mode'.accept_suggestion)
     end
   },
   {
     "Iussaeus/goback.nvim",
-    config = function()
-      local goback = require 'goback'.setup()
-      vim.keymap.set("n", "<leader>gb", function() goback:one_level_back() end)
-      vim.keymap.set("n", "<leader>gf", function() goback:one_level_forth() end)
+    dir = '~/code/goback.nvim',
+    init = function()
+      vim.keymap.set("n", "<leader>gb", require 'goback'.go_back)
+      vim.keymap.set("n", "<leader>gf", require 'goback'.go_forth)
     end
   },
 }
