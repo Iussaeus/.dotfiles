c21c15294da9003791c81fce76dfcb8875356289
Iussaeus <118941666+Iussaeus@users.noreply.github.com>
2026-06-30 22:57:37 +0300

    Modify nvim config

--- stow/nvim/.config/nvim/plugin/other.lua
+++ stow/nvim/.config/nvim/plugin/other.lua
@@ -2,9 +2,9 @@ vim.pack.add({ 'https://github.com/nvim-treesitter/nvim-treesitter' })
 vim.pack.add({ 'https://github.com/kylechui/nvim-surround' })
 
 vim.pack.add({ 'https://github.com/chrisgrieser/nvim-spider' })
-vim.keymap.set({ 'n', 'o', 'x' }, 'w', "<cmd>lua require('spider').motion('w')<CR>")
-vim.keymap.set({ 'n', 'o', 'x' }, 'e', "<cmd>lua require('spider').motion('e')<CR>")
-vim.keymap.set({ 'n', 'o', 'x' }, 'b', "<cmd>lua require('spider').motion('b')<CR>")
+vim.keymap.set({ 'n', 'o', 'x' }, 'w', function() require('spider').motion('w') end)
+vim.keymap.set({ 'n', 'o', 'x' }, 'e', function() require('spider').motion('e') end)
+vim.keymap.set({ 'n', 'o', 'x' }, 'b', function() require('spider').motion('b') end)
 
 vim.cmd.packadd 'nvim.undotree'
 vim.keymap.set({ 'n', 'x' }, '<m-u>', vim.cmd.Undotree)
