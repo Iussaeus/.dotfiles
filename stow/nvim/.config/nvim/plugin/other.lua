0ef80127caa03bf26acbf228ab2dde7e53d4e6aa
Iussaeus <118941666+Iussaeus@users.noreply.github.com>
2026-09-04 17:29:54 +0300

    [nvim] remove hard-time, add firenvim

--- stow/nvim/.config/nvim/plugin/other.lua
+++ stow/nvim/.config/nvim/plugin/other.lua
@@ -1,13 +1,16 @@
 vim.pack.add { 'https://github.com/nvim-treesitter/nvim-treesitter' }
 vim.pack.add { 'https://github.com/kylechui/nvim-surround' }
 vim.pack.add { 'https://github.com/nvim-treesitter/nvim-treesitter-context' }
-vim.pack.add { 'https://github.com/m4xshen/hardtime.nvim' }
-require("hardtime").setup({
-  disabled_keys = {
-    ["<Up>"] = false,
-    ["<Down>"] = false,
-  }
-})
+vim.pack.add { 'https://github.com/MunifTanjim/nui.nvim' }
+
+vim.pack.add { 'https://github.com/glacambre/firenvim' }
+vim.cmd [[ :call firenvim#install(0) ]]
+if vim.g.started_by_firenvim then
+  vim.o.guifont = 'CaskaydiaCove Nerd Font Mono:h9'
+  vim.o.sidescrolloff = 0
+  vim.o.scrolloff = 0
+  vim.opt.signcolumn = "no"
+end
 
 vim.cmd.packadd 'nvim.undotree'
 vim.keymap.set({ 'n', 'x' }, '<m-u>', vim.cmd.Undotree)
