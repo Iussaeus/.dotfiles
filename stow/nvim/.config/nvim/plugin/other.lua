d806849aa8ca3b5e838c34312045b5a9fc4c08ad
Iussaeus <118941666+Iussaeus@users.noreply.github.com>
2026-09-09 18:19:08 +0300

    [nvim] update firenvim config

--- stow/nvim/.config/nvim/plugin/other.lua
+++ stow/nvim/.config/nvim/plugin/other.lua
@@ -6,10 +6,36 @@ vim.pack.add { 'https://github.com/MunifTanjim/nui.nvim' }
 vim.pack.add { 'https://github.com/glacambre/firenvim' }
 vim.cmd [[ :call firenvim#install(0) ]]
 if vim.g.started_by_firenvim then
+  vim.api.nvim_create_user_command('W', function()
+    vim.cmd([[silent! write]])
+    vim.cmd([[echo ""]]) -- Instantly clears any leftover flash
+  end, {})
+
+  vim.cmd([[cnoreabbrev w W]])
+  vim.cmd([[cnoreabbrev write W ]])
+
+  vim.keymap.set('n', 'u', ':silent undo<CR>', { silent = true, noremap = true })
+  vim.keymap.set('n', '<C-r>', ':silent redo<CR>', { silent = true, noremap = true })
+
+  vim.defer_fn(function()
+    vim.opt.laststatus = 0
+    vim.opt.shortmess = "aAsStToOFc"
+    vim.opt.showmode = false
+    vim.opt.cmdheight = 0
+    vim.opt.showcmd = false
+    vim.o.sidescrolloff = 0
+    vim.o.scrolloff = 0
+    vim.opt.signcolumn = "no"
+  end, 10)
+
+  vim.defer_fn(function()
+    if vim.o.lines < 5 then
+      vim.o.lines = 2
+    end
+  end, 70)
+
+  vim.keymap.set({ 'n', 'i' }, '<c-e>', '<cmd>:wq<cr>')
   vim.o.guifont = 'CaskaydiaCove Nerd Font Mono:h9'
-  vim.o.sidescrolloff = 0
-  vim.o.scrolloff = 0
-  vim.opt.signcolumn = "no"
 end
 
 vim.cmd.packadd 'nvim.undotree'
